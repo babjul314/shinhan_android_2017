3abd0c04bf6efabfbeb139af19097c3f622dc99e
IC-INTPC-087098\IC-INTPC-087098 <test@test.net>
2017-03-16 17:33:23 +0900

    HelloWorld 예제 ActivityExam 예제

---
+++ ActivityExam/app/src/main/java/com/shinhan/activityexam/MainActivity.java
@@ -0,0 +1,36 @@
+package com.shinhan.activityexam;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onButton1Clicked(View view) {
+        Log.i("onButton1Clicked","-----------------!!!!!");
+        EditText editText = (EditText)findViewById(R.id.edittext);
+        String string = editText.getText().toString();
+        Log.i("editText", string);
+        Intent intent = new Intent(MainActivity.this, SubActivity.class);
+        intent.putExtra("String", string);
+        startActivityForResult(intent, 0);//startActivity(intent);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 0) {//SubActivity가 종료되었으면
+            if (resultCode == RESULT_OK) {//값을 넘기는 정상 종료일때만
+                String result = data.getStringExtra("Result");
+                EditText editText = (EditText)findViewById(R.id.edittext);
+                editText.setText(result);
+            }
+        }
+    }
+}
---
+++ HelloWorld/app/src/main/java/com/shinhan/helloworld/MainActivity.java
@@ -0,0 +1,45 @@
+package com.shinhan.helloworld;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button)findViewById(R.id.button);
+        TextView textView = (TextView)findViewById(R.id.textview);
+        textView.setText("안녕,신한은행!");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "버튼 클릭!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void onButton2Clicked(View view) {
+        Toast.makeText(MainActivity.this, "두번째 버튼 클릭",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    public void onButton3Clicked(View view) {//웹브라우저 호출
+        Intent intent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("http://m.shinhan.com"));
+        startActivity(intent);
+    }
+    public void onButton4Clicked(View view) {//전화걸기 호출
+        Intent intent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("tel:010-1234-5678"));
+        startActivity(intent);
+    }
+}
