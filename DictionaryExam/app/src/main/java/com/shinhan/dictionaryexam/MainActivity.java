da915c69ae9c9b3e05ef9556b483c72e38d9ab71
IC-INTPC-087098\IC-INTPC-087098 <test@test.net>
2017-03-24 16:02:37 +0900

    ListView 클릭 이벤트 처리 추가

--- DictionaryExam/app/src/main/java/com/shinhan/dictionaryexam/MainActivity.java
+++ DictionaryExam/app/src/main/java/com/shinhan/dictionaryexam/MainActivity.java
@@ -7,9 +7,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -51,5 +53,12 @@ public class MainActivity extends AppCompatActivity {
                 android.R.layout.simple_list_item_1, words);
         ListView listView = (ListView)findViewById(R.id.listview);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, position+"",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
